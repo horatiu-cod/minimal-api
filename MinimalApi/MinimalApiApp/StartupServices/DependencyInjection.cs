7ba345201f4251ec1ad2d6fe753fdadf970ce531
horatiu-cod <53142289+horatiu-cod@users.noreply.github.com>
2023-01-31 14:19:27 +0200

    Refactor program.cs

---
+++ MinimalApi/MinimalApiApp/StartupServices/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MinimalApiApp.StartupServices;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder;
+    }
+}
